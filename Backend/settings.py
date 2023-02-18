f98ebdfea5ed02736ac0acd7e527ac0ff3341bcc
gkprasanth <gkpc2004@gmail.com>
2023-02-18 20:05:03 +0530

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-     'rest_framework',
-     'home'
+    'rest_framework',
+    'home'
 ]
 
 MIDDLEWARE = [
